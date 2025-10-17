5f3f7298cbccc4af667ee2446ccc314404251afb
Takuya-Mukai <takuya.mukai@gmail.com>
2025-10-17 14:12:39 +0900

    Initialization

---
+++ 03/assn.py
@@ -0,0 +1,96 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Assn-3
+#
+# **Goal**: Use Jupyter to reproduce the figures below including:
+#
+# * Colors
+# * Labels
+# * etc.
+
+# %% [markdown]
+# ___
+#
+# ### Figure 1
+#
+# Use `data-fig1.npy` for this figure.
+
+# %%
+# %matplotlib inline
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+a   = np.load( 'data-fig1.npy' )
+
+# enter additional commands here to reproduce the figure below
+
+# NOTE!
+# - Entering commands and running the cell may cause the figure to disapper
+# - If the figure disappears, use Assn-3.html as a reference
+
+# %%
+
+# %% [markdown]
+# ___
+#
+# ### Figure 2
+#
+# Use `data-fig2.npy` for this figure.
+
+# %%
+
+# %% [markdown]
+# ___
+#
+# ### Figure 3
+#
+# Use `data-fig3.npy` for this figure.
+
+# %%
+
+# %% [markdown]
+# ___
+#
+# ### Figure 4
+#
+# Use `data-fig4.npy` for this figure.
+
+# %%
+
+# %% [markdown]
+# ___
+#
+# ### Figure 5
+#
+# Use `data-fig5.npy` for this figure.
+#
+# *Hint*: Use 2D visualization and the "RdBu" colormap.
+
+# %%
+
+# %% [markdown]
+# ___
+#
+# ### UNGRADED BONUS QUESTION
+#
+# (This question is an *optional*, *ungraded* bonus problem.) 
+#
+# Use `data-fig-bonus-X.npy`, `data-fig-bonus-Y.npy` and `data-fig-bonus-Z.npy` for this figure.
+#
+# *Hint*: Search Matplotlib's online examples.
+
+# %%
